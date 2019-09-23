d30fc10734be7a5b1630a91661d3bf2d5d997d48
your username <vitor13m2003@gmail.com>
2019-09-22 23:51:26 -0300

    login feito; ultimas atualizações da vitoria;

--- js/script.js
+++ js/script.js
@@ -3,54 +3,51 @@ var login = document.getElementById("login-screen");
 var singScreen = document.getElementById("sign-screen");
 var address = document.getElementById("address");
 var option;
-function searchEmail(){
-    const email = $("#email").val();
-    var exists;
-    //Realizar busca no banco de dados para verificar se o email existe
- 
-    if (email != ""){
-
-        // Informações iniciais somem
-        init.style.display = "none";
-        //Ajax 
-        $.ajax({
-            url : './php/searchEmail.php',
-            type: 'post',
-            data: {
-                email : email
-            }
-        }).done(function(res){
-            res = JSON.parse(res);
-            exists = res.exists;
-            //exists = 1 TRUE
-            //exists = 0 FALSE
-            if (exists == '0'){
-                login.style.display = "none";
-                singScreen.style.display = "block";
-            }else{
-                
-                singScreen.style.display = "none";
-                login.style.display = "block";
-                $("user").html(res.name);
-            }
-        });
-
-    }
-}
 
 function cpfValidate(){
-    var cpf = document.getElementById("cpf");
-    var span = document.getElementById("span-invalid");
-
-    if (cpf.value != "vitorinha"){
-        span.innerHTML = cpf.id.toUpperCase() + " inválido";
-    }
+    var cpf = $('#cpf').val();
+    cpf = cpf.replace(/[^\d]+/g,'');	
+	if(cpf == '') return false;	
+	// Elimina CPFs invalidos conhecidos	
+	if (cpf.length != 11 || 
+		cpf == "00000000000" || 
+		cpf == "11111111111" || 
+		cpf == "22222222222" || 
+		cpf == "33333333333" || 
+		cpf == "44444444444" || 
+		cpf == "55555555555" || 
+		cpf == "66666666666" || 
+		cpf == "77777777777" || 
+		cpf == "88888888888" || 
+		cpf == "99999999999")
+			return false;		
+	// Valida 1o digito	
+	add = 0;	
+	for (i=0; i < 9; i ++)		
+		add += parseInt(cpf.charAt(i)) * (10 - i);	
+		rev = 11 - (add % 11);	
+		if (rev == 10 || rev == 11)		
+			rev = 0;	
+		if (rev != parseInt(cpf.charAt(9)))		
+			return false;		
+	// Valida 2o digito	
+	add = 0;	
+	for (i = 0; i < 10; i ++)		
+		add += parseInt(cpf.charAt(i)) * (11 - i);	
+	rev = 11 - (add % 11);	
+	if (rev == 10 || rev == 11)	
+		rev = 0;	
+	if (rev != parseInt(cpf.charAt(10)))
+		return alert ('invalido')
+	return alert ('valido')
 }
 
 function showAddress(){
     singScreen.style.overflow = "auto";
     address.style.display = "block";
-    //ajax para buscar as informaçõe de endereço
+
+    $("#sign-screen").animate({scrollTop: $("#sign-screen").height()+170}, 2000);
+
     $.ajax({
         url: 'https://viacep.com.br/ws/'+$('#cep').val()+'/json/unicode/',
 
@@ -65,43 +62,129 @@ function showAddress(){
 
     });
 
-    var objDiv = $("#sign-screen");
-    
-
-    $('#sign-screen').animate({scrollTop: objDiv.scrollHeight}, "low");
-
 }
 
 function radioCheck(option_){
     var formPhy = document.getElementById("form-physical");
     var formLeg = document.getElementById("form-legal");
+    var telLeg = document.getElementById("legal-tel");
+    var telCel = document.getElementById("telefone");
     option = option_;
+    var fields = [... document.querySelectorAll(".field")]
+    fields.forEach(field =>{
+        if(field.id != 'email'){
+            field.value = "";
+        }
+        
+    })
+    var wrongFields = [... document.querySelectorAll(".wrong-field")];
+    wrongFields.forEach(wrongf => {
+        wrongf.classList.remove("wrong-field");
+        wrongf.style.border = "none";
+    })
+
     address.style.display = "none";
     if (option == "juridica"){
         formPhy.style.display = "none";
         formLeg.style.display = "block";
         singScreen.style.overflow = "auto";
+        telLeg.style.display = "block";
+        telCel.placeholder = "Telefone celular";
         
     }else{
-        singScreen.style.overflow = "none";
+        singScreen.style.overflow = "hidden";
         formPhy.style.display = "block";
         formLeg.style.display = "none";
+        telLeg.style.display = "none";
+        telCel.placeholder = "Telefone";
     } 
 }
-const fields = [... document.querySelectorAll(".field")];
+function searchEmail(){
+    const email = $('#email').val();
 
-fields.forEach(field => {
-    field.addEventListener("blur", event => {
-        if (field.value == ""){
-            field.classList.add("wrong-field");
-            field.style.border = "3px solid rgba(255,0,0,0.8)";
-        }else{
-            field.style.border = "none";
-        }
+    var wrongFields = [... document.querySelectorAll(".wrong-field")];
+    wrongFields.forEach(wrongf => {
+        wrongf.classList.remove("wrong-field");
+        wrongf.style.border = "none";
     });
 
-});
+    if (email != ""){
+        init.classList.add("init-fade");
+        load.style.display = "none";   
+        init.addEventListener("animationend", event => {
+            if (event.animationName == "fade"){
+                init.style.display = "none";
 
+                // IR PARA CADASTRO
+                $.ajax({
+                    url : './php/searchEmail.php',
+                    type: 'post',
+                    data: {
+                        email : email
+                    }
+                }).done(function(res){
+                    res = JSON.parse(res);
+                    exists = res.exists;
+                    //exists = 1 TRUE
+                    //exists = 0 FALSE
+                    if (exists == '0'){
+                        login.style.display = "none";
+                        singScreen.style.display = "block";
+                            
+                        singScreen.classList.add("form-show");
+                        init.classList.remove("init-fade");
+                    }else{
+                        
+                        singScreen.style.display = "none";
+                        login.style.display = "block";
+        
+                        login.classList.add("form-show");
+                        init.classList.remove("init-fade");
+                        $("user").html(res.name);
+                    }
+                });
+                var wrongFields = [... document.querySelectorAll(".wrong-field")];
+                wrongFields.forEach(wrongf => {
+                wrongf.classList.remove("wrong-field");
+                wrongf.style.border = "none";
+        });
+            }
+        });
+        
+        
+        $.ajax({
+            url : './php/searchEmail.php',
+            type: 'post',
+            data: {
+                email : email
+            }
+        }).done(function(res){
+            res = JSON.parse(res);
+            exists = res.exists;
+            //exists = 1 TRUE
+            //exists = 0 FALSE
+            if (exists == '0'){
+                init.style.display = "none";
+                login.style.display = "none";
+                singScreen.style.display = "block";
+                    
+                singScreen.classList.add("form-show");
+                init.classList.remove("init-fade");
+            }else{
+                init.style.display = "none";
+                singScreen.style.display = "none";
+                login.style.display = "block";
+
+                login.classList.add("form-show");
+                init.classList.remove("init-fade");
+                $("user").html(res.name);
+            }
+        });
+       
+    }else{
+        load.style.display = "none";
+    }
+}
 function registerClient() {
 
     const logradouro = $('[name=logradouro]').val();
@@ -178,14 +261,12 @@ function registerClient() {
                 // telefone : telefone
             }
         }).done(function(res) {
-            alert(res);
+            // alert(res);
         });
     }
     
 
 }
-
-
 function signIn(){
     const email = $('#email').val();
     const senha = $('#password').val();
@@ -204,5 +285,15 @@ function signIn(){
         alert(res.id);
     });
 }
+// BLOQUEAR ENTRADA DE CARACTERES != DE NUMÉRICOS
+const wordField = [... document.querySelectorAll(".word-field")];
+var nums = [0,1,2,3,4,5,6,7,8,9]
 
-
+wordField.forEach(wfields => {
+    wfields.addEventListener("keypress", event => {
+        var key = event.key;
+        nums.forEach(num => {
+            if (key == num) event.preventDefault();
+        })
+    })
+})
--- style/style.js
+++ style/style.js
@@ -1,6 +1,12 @@
 const child = document.querySelector(".child");
 const form = document.querySelector("form");
 const slider = document.querySelector("#slider");
+var init = document.getElementById("init");
+var login = document.getElementById("login-screen");
+var singScreen = document.getElementById("sign-screen");
+var address = document.getElementById("address");
+const email = document.getElementById("email");
+var load = document.getElementById("load");
 
 child.addEventListener("animationend", event => {
     if (event.animationName == "arising"){
@@ -8,3 +14,74 @@ child.addEventListener("animationend", event => {
         slider.classList.add("separation-Slider");
     }
 });
+
+form.addEventListener("animationend", event => {
+    if (event.animationName == "separationForm")
+        email.focus();
+})
+
+email.addEventListener("keydown", event =>{
+    load.style.display = "block";
+})
+
+function countTime(){
+    if (email.value != ""){
+        init.classList.add("init-fade");
+        load.style.display = "none";   
+        init.addEventListener("animationend", event => {
+            if (event.animationName == "fade"){
+                init.style.display = "none";
+
+                // IR PARA CADASTRO
+                if (email.value != "heyvitoria.lopes@gmail.com"){
+                    login.style.display = "none";
+                    singScreen.style.display = "block";
+                    
+                    singScreen.classList.add("form-show");
+                    init.classList.remove("init-fade");
+
+                // IR PARA WELCOME
+                }else{   
+                    singScreen.style.display = "none";
+                    login.style.display = "block";
+
+                    login.classList.add("form-show");
+                    init.classList.remove("init-fade");
+                }
+            }
+        });
+        // IR PARA CADASTRO
+        if (email.value != "heyvitoria.lopes@gmail.com"){
+            init.style.display = "none";
+            login.style.display = "none";
+            singScreen.style.display = "block";
+            
+            singScreen.classList.add("form-show");
+            init.classList.remove("init-fade");
+
+        // IR PARA WELCOME
+        }else{   
+            init.style.display = "none";
+            singScreen.style.display = "none";
+            login.style.display = "block";
+
+            login.classList.add("form-show");
+            init.classList.remove("init-fade");
+        }
+    }
+}
+
+const fields = [... document.querySelectorAll(".required-field")];
+
+fields.forEach(field => {
+    field.addEventListener("blur", event => {
+        if (field.value == ""){
+            field.classList.add("wrong-field");
+            field.style.border = "3px solid rgba(255,0,0,0.8)";
+        }else{
+            field.style.border = "none";
+        }
+    });
+
+});
+
