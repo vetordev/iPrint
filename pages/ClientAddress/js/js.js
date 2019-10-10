53ff5cfd9041450cde01a8a78dd5b3704d258b52
unknown <heyvitoria.lopes@gmail.com>
2019-10-09 21:33:42 -0300

    load 3 endereços pronto

--- pages/ClientAddress/js/js.js
+++ pages/ClientAddress/js/js.js
@@ -1,5 +1,8 @@
 window.onload = function onload(){
-    loadAddresses(['06567000,Avenida qJosé Lavechia,62,Bairro,São Paulo,SP,apto 102 c']);
+    // loadAddresses(['06567000,Avenida qJosé Lavechia,62,Bairro,São Paulo,SP,apto 102 c,', '06567000,Avenida qJosé Lavechia,62,Bairro,São Paulo,SP,apto 102 c', '06567000,Avenida qJosé Lavechia,62,Bairro,São Paulo,SP,apto 102 c']);
+    // loadAddresses(['06567000,Avenida qJosé Lavechia,62,Bairro,São Paulo,SP,apto 102 c,', '06567000,Avenida qJosé Lavechia,62,Bairro,São Paulo,SP,apto 102 c']);
+    loadAddresses(['06567000,Avenida qJosé Lavechia,62,Bairro,São Paulo,SP,apto 102 c,']);
+
 }
 
 function loadAddresses(address) {
@@ -23,10 +26,105 @@ function loadAddresses(address) {
 
     if (address.length == 2){
         var campos2 = address[1].split(',');
+        createElement(campos2);
+    }
+    else if (address.length == 3) {
+        var campos2 = address[1].split(',');
+        var campos3 = address[2].split(',');
 
-        
+        createElement(campos2);
+        createElement(campos3);
     }
 }
+
+function createElement(info) {
+    var idDel = Math.random()+1;
+    var boxId = 'end' + idDel;
+
+    const divmae = document.getElementById("divmae");
+    const divQtd =  document.getElementsByClassName('box-address').length;
+
+    var newBox = document.createElement('div');
+    var newInfo = document.createElement('div');
+    var newEndereco = document.createElement('p');
+    var newLogr = document.createElement('span');
+    var newCep = document.createElement('span');
+    var newNum = document.createElement('span');
+    var newBairro = document.createElement('span');
+    var newCidade = document.createElement('span');
+    var newUf = document.createElement('span');
+    var newCompl = document.createElement('span');
+    var newbr = document.createElement('br');
+    var newbr2 = document.createElement('br');
+    var newbr3 = document.createElement('br');
+    var newB = document.createElement('b');
+    var newBtnBox = document.createElement('div');
+    var newDivFlex = document.createElement('div');
+    var newBtn = document.createElement('button');
+    var newBtn2 = document.createElement('button');
+    var newVirg = document.createElement('span');
+    var newTraco = document.createElement('span');
+    var newTraco2 = document.createElement('span');
+
+    
+    const show = [newCep, newLogr, newNum, newBairro, newCidade, newUf, newCompl];
+
+    newBox.setAttribute('class', 'box-address client-address');
+    newBox.setAttribute('id', idDel);
+    newBox.setAttribute('onclick', 'mainAddress(id)');
+        newInfo.setAttribute('class', 'info-address');
+            newEndereco.setAttribute('p', 'endereco');
+        // itens dentro de info-address adicionados no for
+
+        newBtnBox.setAttribute('class', 'btn-box');
+            newDivFlex.setAttribute('class', 'div-flex div-flex-h');
+                newBtn.setAttribute('type', 'button');
+                newBtn.setAttribute('class', 'btn-editar');
+                newBtn.setAttribute('onclick', "editAddress('"+boxId+"')")
+                newBtn2.setAttribute('type', 'button');
+                newBtn2.setAttribute('id', idDel);
+                newBtn2.setAttribute('onclick', 'deleteAddress(id)');
+                newBtn2.setAttribute('class', 'btn-del btn btn-danger');
+
+
+    if (divQtd < 4){
+        newBtn.innerHTML = "Editar";
+        newBtn2.innerHTML = "Excluir";
+        newB.innerHTML = "CEP: ";
+        newVirg.innerHTML = ", ";
+        newTraco.innerHTML = " - ";
+        newTraco2.innerHTML = " - ";
+        // newbr.innerHTML = "\n";
+
+        divmae.appendChild(newBox);
+        newBox.appendChild(newInfo);
+        newInfo.appendChild(newEndereco);
+        for (var i = 0; i < info.length; i++) {
+            show[i].innerHTML = info[i];
+            show[i].setAttribute('class', 'info '+boxId);
+        }
+        newEndereco.appendChild(newB);
+        newEndereco.appendChild(show[0]);
+        newEndereco.appendChild(newbr2);
+        newEndereco.appendChild(show[1]);
+        newEndereco.appendChild(newVirg);
+        newEndereco.appendChild(show[2]);
+        newEndereco.appendChild(newTraco2);
+        newEndereco.appendChild(show[3]);
+        newEndereco.appendChild(newbr3);
+        newEndereco.appendChild(show[4]);
+        newEndereco.appendChild(newTraco);
+        newEndereco.appendChild(show[5]);
+        newEndereco.appendChild(newbr);
+        newEndereco.appendChild(show[6]);
+
+        newBox.appendChild(newBtnBox);
+        newBtnBox.appendChild(newDivFlex);
+        newDivFlex.appendChild(newBtn);
+        newDivFlex.appendChild(newBtn2);
+    }
+}
+
 
 function showAddAddress() {
     var formAddress = document.getElementById('form-address');
