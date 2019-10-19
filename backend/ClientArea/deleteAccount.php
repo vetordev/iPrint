ba5c9a4a155aa445b3f56c1b11c6470e90bbfc48
your username <vitor13m2003@gmail.com>
2019-10-19 20:20:58 -0300

    delete account ok

---
+++ backend/ClientArea/deleteAccount.php
@@ -0,0 +1,29 @@
+<?php
+
+require '../php/connection.php';
+
+$user_id = $_POST['type'];
+$type = $_POST['user_id'];
+
+
+$res = deleteAccount($user_id, $type, $con);
+
+echo json_encode($res);
+
+function deleteAccount($user_id, $type, $con) {
+  if($type == 'physical'){
+    $stmt = $con->prepare("DELETE FROM `tb_fisica` WHERE `id_pf` = ?");
+    $stmt->bindParam(1, $user_id);
+    if ($stmt->execute()) {
+      return 'ok';
+    }
+
+  }else{
+    $stmt = $con->prepare("DELETE FROM `tb_juridica` WHERE `id_pj` = ?");
+    $stmt->bindParam(1, $user_id);
+    if ($stmt->execute()) {
+      return 'ok';
+    }
+  }
+}
+?>
--- frontend/pages/ClientArea/js/deleteAccount.js
+++ frontend/pages/ClientArea/js/deleteAccount.js
@@ -1,7 +1,30 @@
 window.addEventListener('load', () => {
-  $('.del-account').click(deleteAccount);
+  $('.del-account').click(() => {
+    const user_id = localStorage.getItem('user_id');
+    const type = localStorage.getItem('type');
+    deleteAccount(user_id, type)
+  });
 });
 
-const deleteAccount = () => {
-  alert('exlcuir');
+const deleteAccount = (user_id, type) => {
+  $.ajax({
+    url: '../../../backend/ClientArea/deleteAccount.php',
+    type: 'post',
+    data : {
+      user_id : user_id,
+      type : type
+    }
+  }).done(res => {
+    try {
+      res = JSON.parse(res);
+      if(res === "ok"){
+        // localStorage.clear();
+        // window.location.href = "../Home/"
+        alert('deu certo cachorro');
+
+      }
+    }catch{
+      alert(res);
+    }
+  });
 }
