2fbf9f0e01f8a661f95bdbb63e5c5b98587a8505
your username <vitor13m2003@gmail.com>
2019-10-21 22:42:08 -0300

    backend updateAccount ok; rever deleteAccont

--- backend/ClientAccount/updateAccount.php
+++ backend/ClientAccount/updateAccount.php
@@ -3,21 +3,45 @@
 require '../php/connection.php';
 
 $type = $_POST['type'];
+$user_id = $_POST['user_id'];
 
+#Pessoa física
 if($type == 'physical'){
-  $user_id = $_POST['user_id'];
+
+  #Dados
   $nome = $_POST['nome'];
   $ddn = $_POST['ddn'];
   $tel = $_POST['telefone'];
 
   #Sem mudar a senha
-  function updateAccountWithoutPassword($user_id, $nome, $ddn, $tel, $con){
-    $stmt = $con->prepare("UPDATE `tb_fisica` SET `nome_pf` = ?, `senha_pf` = ?, `ddn_pf` = ?, `telefone_pf` = ? WHERE `id_pf` = ?");
-  
+  function updateAccountPhysicalWithoutPassword($user_id, $nome, $ddn, $tel, $con){
+    $stmt = $con->prepare("UPDATE `tb_fisica` SET `nome_pf` = ?, `ddn_pf` = ?, `telefone_pf` = ? WHERE `id_pf` = ?");
+    $stmt->bindParam(1, $nome);
+    $stmt->bindParam(2, $ddn);
+    $stmt->bindParam(1, $tel);
+    $stmt->bindParam(1, $user_id);
+    $stmt->execute();
   }
 
 }else{
+  #Dados
+  $nome_fant = $_POST['nome_fant'];
+  $nome_resp = $_POST['nome_resp'];
+  $tel = $_POST['telefone'];
+  $tel_res = $_POST['telefone_res'];
+  $tel_comerc = $_POST['telefone_comerc'];
 
+  #Sem mudar a senha
+  function updateAccountLegalWithoutPassword($user_id, $nome_fant, $nome_resp, $tel, $tel_res, $tel_comerc, $con){
+    $stmt = $con->prepare("UPDATE `tb_juridica` SET `nomeFant_pj` = ?, `nomeResp_pj` = ?, `telCel_pj` = ?, `telResid_pj` = ?, `telComerc_pj` = ? WHERE `id_pj` = ?");
+    $stmt->bindParam(1, $nome_fant);
+    $stmt->bindParam(2, $nome_resp);
+    $stmt->bindParam(3, $tel);
+    $stmt->bindParam(4, $tel_res);
+    $stmt->bindParam(5, $tel_comerc);
+    $stmt->bindParam(6, $user_id);
+    $stmt->execute();
+  }
 }
 
 
