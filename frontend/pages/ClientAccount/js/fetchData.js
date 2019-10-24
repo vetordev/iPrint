b60140c5cd10b6faf7961d373204e3f3a61d3d4d
your username <vitor13m2003@gmail.com>
2019-10-24 20:29:19 -0300

    buscando dados do usuários

--- frontend/pages/ClientAccount/js/fetchData.js
+++ frontend/pages/ClientAccount/js/fetchData.js
@@ -10,6 +10,32 @@ window.addEventListener('load', load => {
       type : type
     }
   }).done(res => {
-    alert(res);
+    try {
+      res = JSON.parse(res);
+      setInputData(res);
+    } catch (error) {
+      alert(res);
+    }
   });
 });
+
+const setInputData = res => {
+  if(localStorage.getItem('type') == 'physical'){
+    $('#email').val(res.email);
+    $('#name').val(res.nome);
+    $('#cpf').val(res.cpf);
+    $('#ddn').val(res.ddn);
+    $('#telefone').val(res.telefone);
+  }else{
+    $('#email').val(res.email);
+    $('#cnpj').val(res.cnpj);
+    $('#rs').val(res.rs);
+    $('#name-f').val(res.nomeF);
+    $('#ie').val(res.ie);
+    $('#name-e').val(res.nomeR);
+    $('#telefone-cel').val(res.telCel);
+    $('#telefoneR').val(res.telResid);
+    $('#telefoneCom').val(res.telComerc);
+  }
+  
+}
