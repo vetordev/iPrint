e02a759d3298bc400ef1d99b9786b85c86b80d0b
unknown <vitor13m2003@gmail.com>
2019-10-26 10:06:05 -0200

    add insert user

---
+++ backend/sql/insert.sql
@@ -0,0 +1,5 @@
+USE `iprint`;
+
+INSERT INTO `tb_fisica` VALUES (1, 'vitor@gov', 'e10adc3949ba59abbe56e057f20f883e', '2003-06-12', '952316039', 'vitasso', '48302352845');
+INSERT INTO `tb_clienteEnd` VALUES (1, 1, '06763270', '', 33);
+INSERT INTO `tb_endereco` VALUES (06763270, 'Avenida Doutor José Maciel', 'Jardim Maria Rosa', 'Taboão da Serra', 'SP');
