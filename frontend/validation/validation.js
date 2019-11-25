3126b0c4342069e45cf1e75ac8fa27a2f83c8ed8
unknown <heyvitoria.lopes@gmail.com>
2019-11-25 13:30:37 -0200

    cpf validado

--- frontend/validation/validation.js
+++ frontend/validation/validation.js
@@ -170,7 +170,6 @@ function numCas()
 
 }
 function VerificaCPF (cpf) {
-    alert('a');
     if (vercpf(cpf)) 
     {return true;}else 
     {errors="1";if (errors) alert('CPF NÃO VÁLIDO');
@@ -183,7 +182,7 @@ texto = texto.replace(er, "");
 console.log(texto);
 
     cpf = texto;
-    
+    alert(cpf);
 
 if (cpf.length != 11 || cpf == "00000000000" || cpf == "11111111111" || cpf == "22222222222" || cpf == "33333333333" || cpf == "44444444444" || cpf == "55555555555" || cpf == "66666666666" || cpf == "77777777777" || cpf == "88888888888" || cpf == "99999999999")
 return false;
